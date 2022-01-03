411c00ecd6b78df51a38f0eeaee7bf459143c91d
osnava <66899625+osnava@users.noreply.github.com>
2022-01-02 22:54:58 -0600

    Add files via upload

---
+++ copy_paste_history/ClipboardHistory.py
@@ -0,0 +1,23 @@
+import pyperclip
+from datetime import datetime
+from openpyxl import load_workbook
+import time
+
+time_delay = 0.100
+old_text = ""
+
+while True:
+    if KeyboardInterrupt:
+        actual_text = pyperclip.paste()
+        if actual_text != old_text:
+            wb = load_workbook(filename="clipboard_history.xlsx")
+            ws = wb.worksheets[0]
+            now = datetime.now()  # datetime object containing current date and time
+            date_string = now.strftime("%d/%m/%Y")  # dd/mm/YY
+            time_string = now.strftime("%H:%M")  # hh:mm
+            ws.append([actual_text, date_string, time_string])
+            wb.save("clipboard_history.xlsx")
+            old_text = actual_text
+    time.sleep(time_delay)
+
+
---
+++ copy_paste_history/installer.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+TITLE Installing Clipboard History Tool v0.0.1
+ECHO tool developed By: Oscar Navarrete
+cd %USERPROFILE%
+ECHO Installing all packages...
+pip install pyperclip openpyxl
+ECHO Installation complete!
+START https://www.youtube.com/watch?v=3aaRetbrXJ8
+PAUSE
